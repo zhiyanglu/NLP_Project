3b2096eee9f39e7e457fe55b96504a617377ba57
zhiyanglu <lu_zhiyang@hotmail.com>
2016-04-16 04:27:59 -0700

    frame of question form based on 7 classes

--- src/QuestionGenerator.java
+++ src/QuestionGenerator.java
@@ -13,8 +13,10 @@ public class QuestionGenerator {
 	}
 	
 	EntitySet es;
+	PropertyClassify pc;
 	public QuestionGenerator(){
 		es = new EntitySet("/Users/Lu/Desktop/544_project/sample_instances.owl");
+		pc = new PropertyClassify();
 	}
 	
 	public String nextQuestion(){
@@ -45,9 +47,55 @@ public class QuestionGenerator {
 	
 	
 	public String formQuestion(String entity, String prop){
+		int class_index = -1;
+		for(int i = 0; i < pc.option.length; i++){
+			if(pc.map.get(pc.option[i]).contains(prop)){
+				class_index = i;
+				break;
+			}
+		}
+		if(class_index == -1) return "";
+		
+		ArrayList<String> words = getQuestionForm(class_index);
+		
 		String s = "";
 		s = entity + "的" + prop + "是什么?";
+		
 		return s;
+	}
+	
+	private ArrayList<String> getQuestionForm(int class_index){
+		//ask about human
+		if(class_index == 0){
+			
+		}
+		//ask about location
+		else if(class_index == 1){
+			
+		}
+		//ask about number
+		else if(class_index == 2){
+			
+		}
+		//ask about time
+		else if(class_index == 3){
+			
+		}
+		//ask about object
+		else if(class_index == 4){
+			
+		}
+		//ask about description
+		else if(class_index == 5){
+			
+		}
+		//ask about unknown things
+		else{
+			
+		}
+		
+		
+		return null;
 	}
 
 }
